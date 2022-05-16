c42270d449e3c6830422d706ab8c7d70d8f140fe
lizzochek <littled685@gmail.com>
2022-05-16 11:54:20 +0200

    Adding base components

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import BaseCard from './components/base/BaseCard.vue';
+import BaseButton from './components/base/BaseButton.vue';
+
 const app = createApp(App);
+
+app.component('base-button', BaseButton);
+app.component('base-card', BaseCard);
 
 app.use(router);
 
