bd910eb71e94781b1b08e0e26813c74afbabc4a7
lizzochek <littled685@gmail.com>
2022-05-19 12:01:42 +0200

    Meta tags and nav guard

--- src/router.js
+++ src/router.js
@@ -6,17 +6,34 @@ import NotFound from './components/pages/NotFound.vue';
 import UserAuth from './components/pages/UserAuth.vue';
 import WishList from './components/pages/WishList.vue';
 
+import store from './store/store';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/books' },
     { path: '/books', component: BookView },
     { path: '/books/:id', props: true, component: BookDetails },
-    { path: '/authentication', component: UserAuth },
-    { path: '/wishlist', component: WishList },
-    { path: '/wishlist/:id', component: BookDetails, props: true },
+    { path: '/authentication', component: UserAuth, meta: { requiresUnauth: true } },
+    { path: '/wishlist', component: WishList, meta: { requiresAuth: true } },
+    {
+      path: '/wishlist/:id',
+      component: BookDetails,
+      meta: { requiresAuth: true },
+      props: true,
+    },
     { path: '/:notFound(.*)', component: NotFound },
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.getters['authentication/isLoggedIn']) {
+    next('/authentication');
+  } else if (to.meta.requiresUnauth && store.getters['authentication/isLoggedIn']) {
+    next('/books');
+  } else {
+    next();
+  }
 });
 
 export default router;
